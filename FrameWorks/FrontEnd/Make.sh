e704e88c69f612a5da110b6ab18c921347203b1a
Welbert Serra <welberts@gmail.com>
2016-04-09 14:16:24 -0300

    .

--- FrameWorks/FrontEnd/Make.sh
+++ FrameWorks/FrontEnd/Make.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#Esse make deve ser copiado para os projetos que possuem o build por grunt
+#É necessário ter o pacote npm e e ter o grunt -cli instalado
+#sudo apt-get install npm -y
+#sudo ln -s /usr/bin/nodejs /usr/bin/node
+#sudo npm install -g grunt-cli 
+
 sudo npm install
 grunt
